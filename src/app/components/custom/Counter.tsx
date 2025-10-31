da75d68bfc46f295ef4e30d147bf2287ace75ce5
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-10-31 22:09:44 +0600

    updated the counter

--- src/app/components/custom/Counter.tsx
+++ src/app/components/custom/Counter.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Counter() {
-  return <section></section>;
+  const [click, setClick] = useState(0);
+
+  return (
+    <section onClick={() => setClick((prev) => prev + 1)}>{click}</section>
+  );
 }
