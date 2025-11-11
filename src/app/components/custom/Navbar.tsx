3a6976e6cd63236b2ec85c168d1640fb6e9c7d82
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-11-11 12:58:04 +0600

    Updated the Navbar.tsx

--- src/app/components/custom/Navbar.tsx
+++ src/app/components/custom/Navbar.tsx
@@ -11,16 +11,18 @@ export default function Navbar() {
         <div className="flex gap-2">
           <Link
             href={"/"}
-            className="flex text-2xl justify-center items-center font-bold gap-2"
+            className="flex text-md md:text-2xl justify-center items-center font-bold gap-2"
           >
-            <TvMinimalPlay className="size-8 text-red-600" /> Youter
+            <TvMinimalPlay className="size-5 md:size-8 text-red-600" /> Youter
           </Link>
         </div>
 
-        <NavbarSearch />
+        <div className="hidden md:flex justify-center items-center">
+          <NavbarSearch />
+        </div>
 
-        <div className="hidden lg:flex justify-center items-center text-[1rem] lg:text-lg">
-          medium man logo
+        <div className="flex justify-center items-center text-[1rem] lg:text-lg">
+          <div className="rounded-full size-8 border"></div>
         </div>
       </div>
     </nav>
